f2a842a0940e055dee157a53782b668c331e9f22
chrisbmx20720 <christian.al.ac20@gmail.com>
2025-01-31 19:54:06 -0600

    backend

--- api/prueba.py
+++
@@ -1,9 +0,0 @@
-
-import os
-
-file_path = "Backend/api/google_credentials/credentials.json"
-
-if os.path.exists(file_path):
-    print("✅ Archivo encontrado")
-else:
-    print("❌ Archivo NO encontrado")
